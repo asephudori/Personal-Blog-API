907e2906929062ef7d541d2f483233d135787929
asep hudori <asep.hudori532@gmail.com>
2024-07-29 14:06:59 +0700

    update app.js

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
-const express = require("express");
-const app = express()
-const port = 3000
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require("morgan")
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(morgan("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+const authRouter = require('./routes/auth.routes');
+const articleRouter = require('./routes/articles.routes');
 const userRouter = require("./routes/users.routes")
-const articleRouter = require("./routes/articles.routes")
 
-app.get('/', (req, res) => {
-    res.send("Selamat datang di Personal blog API")
-})
-
+app.use('/auth', authRouter);
+app.use('/article', articleRouter);
 app.use("/user", userRouter)
-app.use("/article", articleRouter)
+
+app.use('/', (req, res) => {
+  try {
+    let welcomeMessage = {
+      message: 'Welcome to Personal Blog API',
+    };
+    res.json(welcomeMessage);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 app.listen(port, () => {
-    console.log(`listening on port ${port}`)
-})
+  console.log(`listening on port ${port}`);
+});
